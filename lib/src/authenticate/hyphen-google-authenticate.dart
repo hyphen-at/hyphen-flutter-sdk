76c01918f1b6ddffeaaadfe296ba53c742a983ed
Lea Lobanov <lea.lobanov@icloud.com>
2024-02-05 22:25:01 +0800

    set up example app

--- lib/src/authenticate/hyphen-google-authenticate.dart
+++ lib/src/authenticate/hyphen-google-authenticate.dart
@@ -1,36 +1,32 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:hyphen_flutter_sdk/src/authenticate/hyphen-authenticate_delegate.dart';
-// import 'package:hyphen_flutter_sdk/src/core/error/hyphen_sdk_error.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:hyphen_flutter_sdk/src/authenticate/hyphen-authenticate_delegate.dart';
+import 'package:hyphen_flutter_sdk/src/core/error/hyphen_sdk_error.dart';
 
 
-// class HyphenGoogleAuthenticate {
-//   static Future<AuthResult> authenticate(
-//     HyphenAuthenticateDelegate activity,
-//     String webClientId,
-//   ) async {
-//     final options = GoogleSignInOptions(
-//       scopes: ['email'],
-//       clientId: webClientId,
-//     );
+class HyphenGoogleAuthenticate {
+  static Future<UserCredential> authenticate(
+    HyphenAuthenticateDelegate activity,
+    String webClientId,
+  ) async {
 
-//     final googleSignIn = GoogleSignIn(scopes: ['email']);
-//     final googleSignInAccount = await googleSignIn.signIn();
+    final googleSignIn = GoogleSignIn(scopes: ['email'], clientId: webClientId);
+    final googleSignInAccount = await googleSignIn.signIn();
 
-//     try {
-//       final googleSignInAuthentication = await googleSignInAccount.authentication;
-//       final googleSignInCredential = GoogleAuthProvider.credential(
-//         idToken: googleSignInAuthentication.idToken,
-//         accessToken: googleSignInAuthentication.accessToken,
-//       );
+    try {
+      final googleSignInAuthentication = await googleSignInAccount?.authentication;
+      final googleSignInCredential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication?.idToken,
+        accessToken: googleSignInAuthentication?.accessToken,
+      );
 
-//       return await FirebaseAuth.instance.signInWithCredential(googleSignInCredential);
-//     } on FirebaseAuthException catch (e) {
-//       if (e.code == 'user-canceled') {
-//         throw GoogleAuthError;
-//       } else {
-//         rethrow;
-//       }
-//     }
-//   }
-// }
+      return await FirebaseAuth.instance.signInWithCredential(googleSignInCredential);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-canceled') {
+        throw GoogleAuthError;
+      } else {
+        rethrow;
+      }
+    }
+  }
+}
