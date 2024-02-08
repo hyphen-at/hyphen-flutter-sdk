800028eb92ef4e4bad2318f14f2c7d3d5365df2d
Lea Lobanov <lea.lobanov@icloud.com>
2024-02-08 16:23:32 +0800

    Working on method channel setup

--- android/app/src/main/kotlin/com/example/hyphen_flutter_sdk/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hyphen_flutter_sdk/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.hyphen_flutter_sdk
 
 import android.os.Bundle
-import com.example.hyphen_flutter_sdk.crypto.HyphenCryptography
-import com.example.hyphen_flutter_sdk.flow.HyphenFlow
-import com.example.hyphen_flutter_sdk.flow.getAccount
-import com.example.hyphen_flutter_sdk.flow.getAccountKey
-import com.example.hyphen_flutter_sdk.flow.waitForSeal
-import com.nftco.flow.sdk.FlowAddress
+import androidx.lifecycle.lifecycleScope
+import at.hyphen.android.sdk.core.crypto.HyphenCryptography
+import at.hyphen.android.sdk.flow.HyphenFlow
 import com.nftco.flow.sdk.FlowArgument
-import com.nftco.flow.sdk.FlowId
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import kotlinx.coroutines.launch
 
 class MainActivity : FlutterActivity() {
 
@@ -27,127 +24,117 @@ class MainActivity : FlutterActivity() {
         GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
 
         // Set up channel 1
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL_1)
-                .setMethodCallHandler { call, result ->
-                    when (call.method) {
-                        "isDeviceKeyExist" -> {
-                            try {
-                                final bool deviceKeyExists = await HyphenCryptography.isDeviceKeyExist();
-                                result.success(deviceKeyExists);
-                              } catch (e) {
-                                result.error("ERROR_CODE", "Error checking device key existence", null);
-                              }
-                              break;
-                        }
-                        "generateKey" -> {
-                            try {
-                                HyphenCryptography.generateKey();
-                                result.success(true);
-                              } catch (e) {
-                                result.error("ERROR_CODE", "Error generating key", null);
-                              }
-                              break;
-                        }
-                        "getPublicKeyHex" -> {
-                            try {
-                                String publicKeyHex = await HyphenCryptography.getPublicKeyHex();
-                                result.success(publicKeyHex);
-                              } catch (e) {
-                                result.error("ERROR_CODE", "Error getting public key hex", null);
-                              }
-                              break;
-                        }
-                        "signData" -> {
-                            try {
-                                List<int> data = call.arguments["data"];
-                                Uint8List resultData = await HyphenCryptography.signData(data);
-                                result.success(resultData);
-                              } catch (e) {
-                                result.error("ERROR_CODE", "Error signing data", null);
-                              }
-                              break;
-                        }
-                        "encrypt" -> {
-                            try {
-                                List<int> data = call.arguments["data"];
-                                Uint8List resultData = await HyphenCryptography.encrypt(data);
-                                result.success(resultData);
-                              } catch (e) {
-                                result.error("ERROR_CODE", "Error encrypting data", null);
-                              }
-                              break;
-                        }
-                        "decrypt" -> {
-                            try {
-                                List<int> data = call.arguments["data"];
-                                Uint8List resultData = await HyphenCryptography.decrypt(data);
-                                result.success(resultData);
-                              } catch (e) {
-                                result.error("ERROR_CODE", "Error decrypting data", null);
-                              }
-                              break;
-                        }
-                        else -> {
-                            result.notImplemented()
+        flutterEngine?.dartExecutor?.binaryMessenger?.let {
+            MethodChannel(it, CHANNEL_1)
+                    .setMethodCallHandler { call, result ->
+                        when (call.method) {
+                            "isDeviceKeyExist" -> {
+                                try {
+                                    val deviceKeyExists = HyphenCryptography.isDeviceKeyExist();
+                                    result.success(deviceKeyExists);
+                                } catch (e: Exception) {
+                                    result.error("ERROR_CODE", "Error checking device key existence", null);
+                                }
+                            }
+
+                            "generateKey" -> {
+                                try {
+                                    HyphenCryptography.generateKey();
+                                    result.success(true);
+                                } catch (e: Exception) {
+                                    result.error("ERROR_CODE", "Error generating key", null);
+                                }
+                            }
+
+                            "getPublicKeyHex" -> {
+                                try {
+                                    val publicKeyHex = HyphenCryptography.getPublicKeyHex();
+                                    result.success(publicKeyHex);
+                                } catch (e: Exception) {
+                                    result.error("ERROR_CODE", "Error getting public key hex", null);
+                                }
+                            }
+
+                            "signData" -> {
+                                val data = call.argument<List<Int>>("data");
+                                if (data != null) {
+                                    lifecycleScope.launch {
+                                        try {
+                                            val byteArrayData = data.map { it -> it.toByte() }.toByteArray()
+                                            val resultData = HyphenCryptography.signData(byteArrayData)
+                                            result.success(resultData)
+                                        } catch (e: Exception) {
+                                            result.error("ERROR_CODE", "Error signing data", null)
+                                        }
+                                    }
+                                } else {
+                                    result.error("MISSING_ARGUMENTS", "Missing data argument", null)
+                                }
+
+                            }
+
+                            "encrypt" -> {
+                                try {
+                                    val data = call.argument<List<Int>>("data");
+                                    val byteArrayData = data?.map { it -> it.toByte() }?.toByteArray()
+                                    val resultData = byteArrayData?.let { it1 -> HyphenCryptography.encrypt(it1) }
+                                    result.success(resultData);
+                                } catch (e: Exception) {
+                                    result.error("ERROR_CODE", "Error encrypting data", null);
+                                }
+                            }
+
+                            "decrypt" -> {
+                                try {
+                                    val data = call.argument<List<Int>>("data");
+                                    val byteArrayData = data?.map { it -> it.toByte() }?.toByteArray()
+                                    val resultData = byteArrayData?.let { it1 -> HyphenCryptography.decrypt(it1) }
+                                    result.success(resultData);
+                                } catch (e: Exception) {
+                                    result.error("ERROR_CODE", "Error decrypting data", null);
+                                }
+                            }
+
+                            else -> {
+                                result.notImplemented()
+                            }
                         }
                     }
-                }
+        }
         // Set up channel 2
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL_2)
-                .setMethodCallHandler { call, result ->
-                  when (call.method) {
-                    "getAccount" -> {
-                      try {
-                        val address = call.argument<String>("address")
-                        val account = address?.let { FlowAddress(it) }?.let { getAccount(it) }
-                        val response = mapOf("accountName" to account.name)
-                        result.success(response)
-                      } catch (e: Exception) {
-                          result.error("GET_ACCOUNT_ERROR", e.toString(), null)
-                      }
-                    }
-                    "getAccountKey" -> {
-                      try {
-                        val address = call.argument<String>("address")
-                        val keyIndex = call.argument<Int>("keyIndex")
-                        val accountKey = address?.let { FlowAddress(it) }?.let { getAccountKey(it, keyIndex ?: 0) }
-                        val response = mapOf("keyName" to accountKey.name)
-                        result.success(response)
-                      } catch (e: Exception) {
-                          result.error("GET_ACCOUNT_KEY_ERROR", e.toString(), null)
-                      }
-                    }
-                    "waitForSeal" -> {
-                      try {
-                        val txID = call.argument<String>("txID")
-                        val sealedResult = txID?.let { FlowId(it) }?.let { waitForSeal(it) }
-                        val response = mapOf("status" to sealedResult?.status.toString())
-                        result.success(response)
-                      } catch (e: Exception) {
-                          result.error("WAIT_FOR_SEAL_ERROR", e.toString(), null)
-                      }
-                    }
-                    "signAndSendTransaction" -> {
-                      val cadenceScript = call.argument<String>("cadenceScript")
-                      val arguments = call.argument<List<Map<String, Any>>>("arguments")
-                      val withAuthorizer = call.argument<Boolean>("withAuthorizer")
+        flutterEngine?.dartExecutor?.binaryMessenger?.let {
+            MethodChannel(it, CHANNEL_2)
+                    .setMethodCallHandler { call, result ->
+                        when (call.method) {
+                            "signAndSendTransaction" -> {
+                                val cadenceScript = call.argument<String>("cadenceScript")
+                                val arguments = call.argument<List<Map<String, Any>>>("arguments")
+                                val withAuthorizer = call.argument<Boolean>("withAuthorizer")
 
-                      if (cadenceScript != null && arguments != null && withAuthorizer != null) {
-                          val transactionId = HyphenFlow.signAndSendTransaction(
-                              cadenceScript,
-                              arguments as List<FlowArgument>,
-                              withAuthorizer
-                          )
+                                if (cadenceScript != null && arguments != null && withAuthorizer != null) {
+                                    lifecycleScope.launch { // Use lifecycleScope here
+                                        try {
+                                            val transactionId = HyphenFlow.signAndSendTransaction(
+                                                    cadenceScript,
+                                                    arguments as List<FlowArgument>,
+                                                    withAuthorizer
+                                            )
 
-                          result.success(transactionId)
-                      } else {
-                          result.error("MISSING_ARGUMENTS", "Missing required arguments", null)
-                      }
+                                            result.success(transactionId)
+                                        } catch (e: Exception) {
+                                            result.error("TRANSACTION_ERROR", "Error processing transaction", null)
+                                        }
+                                    }
+                                } else {
+                                    result.error("MISSING_ARGUMENTS", "Missing required arguments", null)
+                                }
+                            }
+
+                            else -> {
+                                result.notImplemented()
+                            }
+                        }
                     }
-                    else -> {
-                        result.notImplemented()
-                    }
-                  }
-                }
+        }
     }
 }
--- android/app/src/main/kotlin/com/example/hyphen_flutter_sdk/flow/HyphenFlowCadence.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hyphen_flutter_sdk.flow
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class HyphenFlowCadence(
-        val cadence: String,
-)
