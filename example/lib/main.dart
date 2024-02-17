05bd362e56f1538931929f0c069cbbd23fc35add
Lea Lobanov <lea.lobanov@icloud.com>
2024-02-17 14:29:38 +0800

    Example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -101,14 +101,80 @@ class _MyAppState extends State<MyApp> {
     Hyphen.appSecret = "lH2rU/P0gdv5+zJfgQzITluGHBNtX2jG1JiBZNbKHPQ";
     Hyphen.network = NetworkType.TESTNET;
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: const Center(
-          child: Text('Hyphen Flutter SDK'),
-        ),
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Hyphen SDK Demo',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                const Text(
+                  'This is the sample code for the Hyphen Android SDK. '
+                  'Please refer to the app and implement it in the app.\n\n'
+                  'The Hyphen SDK already includes 2FA verification feature. '
+                  'After installing demo app on another device, you can test it by logging in using the same account.',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: changeNetwork,
+                  child: Text(networkState),
+                ),
+                const SizedBox(height: 32),
+                const Text(
+                  'Login with Google',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: loginWithGoogle,
+                  child: const Text('Login with Google'),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  authText,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 32),
+                const Text(
+                  'Sign & Send Transaction',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: signAndSendTransaction,
+                  child: const Text('Signing & Send Transaction'),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  txText,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          if (isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.2),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
